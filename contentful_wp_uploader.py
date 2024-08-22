86a18c13b07f334b3141aac82db4f5eff80487d2
TootPankake <32971063+TootPankake@users.noreply.github.com>
2024-08-22 21:56:56 +0000

    Added grandparent pages for categories, still not fully connected to the activities or articles.

--- contentful_wp_uploader.py
+++ contentful_wp_uploader.py
@@ -12,8 +12,17 @@ from rich_text_renderer import RichTextRenderer
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from config import SPACE_ID, ACCESS_TOKEN, OPENAI_API_TOKEN, USERNAME, PASSWORD, URI, URL
 
+### category list page -> category detail page -> activity detail page (add article links)
+# makefile or input to do clean sweep or only recent articles, maybe add api tokens as parameters to aws function
+# exponential backoff
+# categories from contentful (only category type of activity) all added as activity
+# already 10+ entries linked to activity in the right tab on contentful (make sure to filter only articles)
+# link to articles instead of barriers -^
+# fill categories with blank descriptions for now, maybe comment out, then later when content is generated it fill them
+# remove other pages for now
+
 clientAI = OpenAI(api_key=OPENAI_API_TOKEN)
-model = "gpt-4o"
+model = "gpt-4o-mini"
 environment = 'development' # Not master
 clientDB = MongoClient(URI, server_api=ServerApi('1'), tlsCAFile=certifi.where())
 db = clientDB["brimming-test"]
@@ -22,9 +31,18 @@ auth = HTTPBasicAuth(USERNAME, PASSWORD)
 api_url = f"{URL}wp-json/wp/v2/pages"
 meta_url = f"{api_url}/{{page_id}}" 
 
-addActivities = input("Do you need to refresh the activity descriptions? (y/n): ").strip().upper()
+try:
+    client = contentful.Client(SPACE_ID, ACCESS_TOKEN,  # Initialize Contentful API Client
+                               environment=environment,
+                               max_include_resolution_depth=1)
+    renderer = RichTextRenderer()  # To render RTF input from contentful
+    date_threshold = datetime(2024, 1, 1).isoformat()
+    date_threshold_categories = datetime(2023, 1, 1).isoformat()
+    print("Successfully connected to Contentful client.")
+except contentful.errors.NotFoundError as e:
+    print(f"Error: {e}")
+  
 refreshArticles = input("Do you need to refresh EVERY article? (y/n): ").strip().upper()
-
 if refreshArticles == 'Y':
     date_threshold_articles = datetime(2023, 1, 1).isoformat()
 else: 
@@ -34,23 +52,13 @@ else:
     collection.delete_many({'_id': {'$in': ids_to_delete}}) # Delete the identified documents
     recent_posts = collection.find().sort("timestamp", -1).limit(1) # Fetch the most recent posts (assuming you have a 'timestamp' field)
     today = {"name": datetime.now(), "created_at": datetime.now()}
-    collection.insert_one(today)
+    collection.insert_one(today) # move to end -^
     for post in recent_posts:
         formattedTime = post['created_at']
         adjusted_time = formattedTime - timedelta(minutes=1) # gives some leeway on last db date request
         formatted_date = adjusted_time.strftime('%Y-%m-%d %H:%M:%S.%f')
     date_threshold_articles = formatted_date
-    clientDB.close()
-    
-try:
-    client = contentful.Client(SPACE_ID, ACCESS_TOKEN,  # Initialize Contentful API Client
-                               environment=environment,
-                               max_include_resolution_depth=1)
-    renderer = RichTextRenderer()  # To render RTF input from contentful
-    date_threshold = datetime(2024, 1, 1).isoformat()
-    print("Successfully connected to Contentful client.")
-except contentful.errors.NotFoundError as e:
-    print(f"Error: {e}")
+    clientDB.close() ### put in the end
     
 def generate_article_links(title, article, slug_list):
     html_output = renderer.render(article)
@@ -94,7 +102,6 @@ def process_article(entry):
     activity = activities_list[0] if activities_list else ''
     barrier = barriers_list[0] if barriers_list else ''
     ai_updated_article = generate_article_links(title, content, json_slug_data)  # Add hyperlinks to the article
-    
     return {
         'title': title,
         'id': id,
@@ -110,7 +117,7 @@ def fetch_all_pages_concurrently():
     page = 1
 
     def fetch_page(page_number):
-        response = requests.get(api_url, params={'per_page': 100, 'page': page_number}, auth=auth)
+        response = requests.get(api_url, params={'per_page': 100, 'page': page_number}, auth=auth, timeout = 10)
         return response.json() if response.status_code == 200 else []
 
     with ThreadPoolExecutor(max_workers=10) as executor:
@@ -124,7 +131,7 @@ def fetch_metadata_id_concurrently(existing_pages):
     def fetch_metadata(page):
         page_id = page['id']
         meta_url = f'{URL}/wp-json/wp/v2/pages/{page_id}'
-        meta_response = requests.get(meta_url, auth=auth)
+        meta_response = requests.get(meta_url, auth=auth, timeout = 10)
         if meta_response.status_code == 200:
             meta_data = meta_response.json().get('meta', {})
             metadata_id = meta_data.get('_metadata_id', None)
@@ -138,7 +145,61 @@ def fetch_metadata_id_concurrently(existing_pages):
             result = future.result()
             if result:
                 existing_metadata.append(result)
-                
+
+def create_grandparent_page(categoryTitle, categoryDescription, entryID):
+    for item in existing_metadata:
+        if entryID == item['metadata_id']:
+            print(f"{entryID} found")
+            page_id = item['id']
+            # Update the page content
+            page_data = {
+                'title': categoryTitle,
+                'content': categoryDescription
+            }
+
+            response = requests.post(meta_url.format(page_id=page_id), json=page_data, auth=auth)
+            if response.status_code == 200:
+                print(f'{categoryTitle} updated successfully')
+                return page_id
+            else:
+                print(f'Failed to update page: {response.status_code}')
+                print(categoryTitle)
+                print(response.json())  # Print the response for debugging
+            return 
+
+    # If no matching metadata ID found, create a new page
+    page_data = {
+        'title': categoryTitle,
+        'content': categoryDescription,
+        'status': 'publish'
+    }
+
+    response = requests.post(api_url, json=page_data, auth=auth)
+
+    # Check if the page creation was successful
+    if response.status_code == 201:
+        print(f'{categoryTitle} created successfully')
+        page_id = response.json()['id']
+
+        # Update the metadata
+        meta_data = {
+            'meta': {
+                '_metadata_id': entryID  # Your metadata ID value
+            }
+        }
+
+        update_meta_response = requests.post(meta_url.format(page_id=page_id), json=meta_data, auth=auth)
+        if update_meta_response.status_code == 200:
+            print('Metadata updated')
+            return page_id
+        else:
+            print(f'Failed to update metadata: {update_meta_response.status_code}')
+            #print(update_meta_response.json())  # Print the response for debugging
+    else:
+        print(f'Failed to create page: {response.status_code}')
+        #print(response.json())  # Print the response for debugging
+    return
+    
 def create_parent_page(activityTitle, activityDescription, entryID):
     for item in existing_metadata:
         if entryID == item['metadata_id']:
@@ -257,48 +318,97 @@ def create_child_page_concurrently(article):
     if article["has_activities_and_barriers"]:
         activity = article["activity"]
         parent_id = parent_page_ids.get(activity, other_page_id)  # Get the parent page ID
-    else:
-        parent_id = other_page_id
 
     create_child_page(article, parent_id)   
 
 skip1 = 0
 skip2 = 0
+skip3 = 0
 iteration = 0
-limit = 100
+limit = 50
+all_categories = []
 all_activities = []
 all_articles = []
 activity_data = []
 article_data = []
 slugs = []
 existing_metadata = []
+print("Fetching metadata ID's")
 existing_pages = fetch_all_pages_concurrently()
 fetch_metadata_id_concurrently(existing_pages)
+print("Fetching Contentful data")
 
-while True:  # Fetch activities with pagination parameters
+while True: # Fetch categories
+    categories = client.entries({
+        'content_type': "category",
+        'limit': limit,
+        'skip': skip1,
+        'order': '-sys.createdAt',
+        'sys.updatedAt[gte]': date_threshold_categories  # limiting Brim activities to after start of 2024
+    })
+    all_categories.extend(categories)
+    skip1 += limit 
+    if len(categories) < limit:  # Break the loop if no more entries are fetched
+        break
+while True:  # Fetch activities 
     activities = client.entries({
         'content_type': "brim",
         'limit': limit,
-        'skip': skip1,
+        'skip': skip2,
         'order': '-sys.createdAt',
         'sys.updatedAt[gte]': date_threshold  # limiting Brim activities to after start of 2024
     })
     all_activities.extend(activities)
-    skip1 += limit 
+    skip2 += limit 
     if len(activities) < limit:  # Break the loop if no more entries are fetched
         break
-while True:  # Fetch articles with pagination parameters
+while True:  # Fetch articles 
     articles = client.entries({  
         'content_type': "article",
         'limit': limit,
-        'skip': skip2,
+        'skip': skip3,
         'order': '-sys.createdAt',
         'sys.updatedAt[gte]': date_threshold_articles
     })
     all_articles.extend(articles)
-    skip2 += limit 
+    skip3 += limit 
     if len(articles) < limit:  # Break the loop if no more articles are fetched
         break 
+
+for entry in all_categories:
+    slug = entry.fields().get('slug')  
+    title = entry.fields().get('title')
+    description = entry.fields().get('description') # change to whatever we choose as the name for description
+    category_type = entry.fields().get('category_type')
+    category_id = entry.sys.get('id')
+    if category_type == "Activity":
+        create_grandparent_page(title, description, category_id)
+        linked_entries = client.entries({
+        'links_to_entry': category_id
+        })
+        limit = 20
+        skip = 0
+        total_fetched = 0
+
+        while True:
+            linked_entries = client.entries({
+                'links_to_entry': category_id,
+                'skip': skip,
+                'limit': limit
+            })
+
+            # Process the fetched entries
+            for linked_entry in linked_entries:
+                content_type = linked_entry.sys.get('content_type')
+                if content_type.id == "brim":  # Check if content type is 'article'
+                    title = linked_entry.fields().get('title', 'No title')
+                    entry_id = linked_entry.sys.get('id', 'Unknown ID')
+                    #print(f"- {title}: {entry_id}")
+            
+            total_fetched += len(linked_entries)
+            skip += len(linked_entries)
+            if len(linked_entries) < limit:
+                break
 for entry in all_articles:
     slug = entry.fields().get('slug')  
     title = entry.fields().get('title')
@@ -344,11 +454,10 @@ for entry in all_activities:
 json_slug_data = json.dumps(slugs)
 json_article_data = json.dumps(article_data, indent=4)
 json_activity_data = json.dumps(activity_data, indent=4)
-print("Collected all contentful data")
-
+print("Collected all Contentful data")
 print(f"Compiling {model} prompts")
 processed_articles = []
-with ThreadPoolExecutor(max_workers=10) as executor: # parallelization of prompt execution
+'''with ThreadPoolExecutor(max_workers=10) as executor: # parallelization of prompt execution
     futures = {executor.submit(process_article, entry): entry for entry in all_articles}
 
     for future in as_completed(futures):
@@ -361,7 +470,12 @@ with ThreadPoolExecutor(max_workers=10) as executor: # parallelization of prompt
             print(f"Exception occurred while processing article: {exc}")
 
 for article in processed_articles:
-    article["content"] = article["content"].replace("[ARTICLE END]", "")        
+    article["content"] = article["content"].replace("[ARTICLE END]", "")  '''      
+
+with open('processed_articles.json', 'r') as file:
+    processed_articles = json.load(file)
+
+print(f"Loaded {len(processed_articles)} processed articles from JSON.")
 
 activity_types = {item['activity'] for item in processed_articles}    
 parent_pages = {}
@@ -373,7 +487,7 @@ for activity in sorted(activity_types):
         content = entry.fields().get('description_full')
         id = entry.sys.get('id')
 
-        if addActivities == 'Y' and title == activity:
+        if title == activity:
             barriers = entry.fields().get('barriers', [])
             barriers_list = [barrier.fields().get('title') for barrier in barriers]
 
@@ -393,9 +507,9 @@ for activity in sorted(activity_types):
                     content += "\n"
                 parent_page_id = create_parent_page(activity, content, id)
                 parent_page_ids[activity] = parent_page_id
+other_page_id = ""
 
-other_page_id = create_parent_page("Other","","0451")
-
+sys.exit()
 with ThreadPoolExecutor(max_workers=10) as executor:
     futures = {executor.submit(create_child_page_concurrently, article): article for article in processed_articles}
 
